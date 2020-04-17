15d0197bc53496c7445fe6734ec673d0d679dc73
Dubwise <34376229+Dubwise56@users.noreply.github.com>
2020-04-17 15:49:46 +0100

    fixed

--- Source/UniversalFermenter/WorkGiver_TakeProductOutOfUniversalFermenter.cs
+++ Source/UniversalFermenter/WorkGiver_TakeProductOutOfUniversalFermenter.cs
@@ -1,40 +1,32 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
 using Verse;
 using Verse.AI;
 
 namespace UniversalProcessors
 {
+    public class WorkGiver_TakeProductOutOfUniversalFermenter : WorkGiver_Scanner
+    {
+        public override PathEndMode PathEndMode => PathEndMode.Touch;
 
-	public class WorkGiver_TakeProductOutOfUniversalFermenter : WorkGiver_Scanner
-	{
-
-		public override ThingRequest PotentialWorkThingRequest
-		{
-			get
-			{
-				return ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial);
-			}
-		}
-
-		public override PathEndMode PathEndMode
-		{
-			get
-			{
-				return PathEndMode.Touch;
-			}
-		}
+        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+        {
+            return CompUniversalFermenter.comps.Where(x => x.parent.Map == pawn.Map).Select(x => x.parent);
+        }
 
 
-		public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
-		{
-			CompUniversalFermenter comp = t.TryGetComp<CompUniversalFermenter>();
-			return comp != null && comp.Fermented && !t.IsBurning() && !t.IsForbidden(pawn) && pawn.CanReserveAndReach(t, PathEndMode.Touch, pawn.NormalMaxDanger(), 1, -1, null, forced);
-		}
+        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            var comp = t.TryGetComp<CompUniversalFermenter>();
+            return comp != null && comp.Fermented && !t.IsBurning() && !t.IsForbidden(pawn) &&
+                   pawn.CanReserveAndReach(t, PathEndMode.Touch, pawn.NormalMaxDanger(), 1, -1, null, forced);
+        }
 
 
-		public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
-		{
-			return new Job(DefDatabase<JobDef>.GetNamed("TakeProductOutOfUniversalFermenter"), t);
-		}
-	}
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return new Job(DefDatabase<JobDef>.GetNamed("TakeProductOutOfUniversalFermenter"), t);
+        }
+    }
 }
